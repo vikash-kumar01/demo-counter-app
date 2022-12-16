8dafaa02a2b9a70efe30618713b06fb029142556
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2022-12-16 12:29:50 +0530

    Update SpringController.java

--- src/main/java/com/example/springboot/controller/SpringController.java
+++ src/main/java/com/example/springboot/controller/SpringController.java
@@ -23,7 +23,7 @@ public class SpringController {
 //		Integer sec = date.getSecond();
 		val++;
 //		Integer count = year + month + day + hour + min + sec + 1;
-		String result = "Hello-World-" + val;
+		String result = "Mr. DevOps" + val;
 		return result;
 	}
 }
